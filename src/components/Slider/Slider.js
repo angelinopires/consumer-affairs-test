cd6e7ae8925219c775d2b53a3339c7825c062048
Angelino Oliveira <angelino.o@turing.com>
2021-10-11 13:01:24 -0300

    fix: layout broken when got an error on inputs

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,24 +1,22 @@
 const sliders = document.querySelectorAll('.slider')
 
-window.addEventListener('load', () => {
-  sliders.forEach((slider) => {
-    const range = slider.querySelector('.slider__range')
-    const thumb = slider.querySelector('.slider__thumb')
-    const track = slider.querySelector('.slider__innerTrack')
+sliders.forEach((slider) => {
+  const range = slider.querySelector('.slider__range')
+  const thumb = slider.querySelector('.slider__thumb')
+  const track = slider.querySelector('.slider__innerTrack')
 
-    const sliderMax = Number(range.getAttribute('max'))
+  const sliderMax = Number(range.getAttribute('max'))
 
-    const updateSlider = (value) => {
-      const percentageValue = (Number(value) / (Number(sliderMax))) * 100
+  const updateSlider = (value) => {
+    const percentageValue = (Number(value) / (Number(sliderMax))) * 100
 
-      thumb.style.left = `${percentageValue}%`
-      thumb.style.transform = `translate(-${percentageValue}%, -50%)`
-      track.style.width = `${percentageValue}%`
-    }
+    thumb.style.left = `${percentageValue}%`
+    thumb.style.transform = `translate(-${percentageValue}%, -50%)`
+    track.style.width = `${percentageValue}%`
+  }
 
-    updateSlider(sliderMax / 2)
-    range.addEventListener('input', (e) => updateSlider(e.target.value))
-  })
+  updateSlider(sliderMax / 2)
+  range.addEventListener('input', (e) => updateSlider(e.target.value))
 })
 
 window.addEventListener('beforeunload', (event) => {
